84791762c6e5ede412c921162daa81e3dbc85a48
zerov83 <zerov83@localhost>
2010-08-19 15:11:47 +0200

    repo settings

---
+++ cfcslib/PDController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cfcslib {
+    public class PDController {
+        private double _kp = 1.0;
+        private double _tv = 1.0;
+
+        private Derviator _diff;
+
+        public PDController(double kp, double tv) {
+            _diff = new Derviator(tv);
+            _kp = kp;
+        }
+
+        public double Calculate(double input) {
+            double y = _diff.Calculate(input);
+            y = _kp * (y + input);
+            return y;
+        }
+    }
+}
