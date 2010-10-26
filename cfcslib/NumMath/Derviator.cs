da5b9bf9a39691fb5e50feac18d394d5bea1163e
zerov83 <zerov83@localhost>
2010-10-26 17:36:32 +0200

    Refactoring

--- cfcslib/NumMath/Derviator.cs
+++ cfcslib/NumMath/Derviator.cs
@@ -1,53 +1,53 @@
 ﻿using System;
 
-using System.Collections.Generic;
-using System.Text;
-
 namespace Cfcslib.NumMath {
     /// <summary>
     /// Berechnet die Ableitung (Steigung)
     /// </summary>
     public class Derviator {
-        /// <summary>
-        /// Verstrichene Zeit seit letztem Aufruf
-        /// </summary>
-        private TimeSpan _tc;
+        private readonly double _k;
+        private bool _init;
+
         /// <summary>
         /// Zeitpunkt letzter Berechnung
         /// </summary>
         private DateTime _last;
 
         private double _old;
-        private bool _init;
 
         private double _out;
-        private double _k;
 
-        public Derviator() :this(1.0){}
+        /// <summary>
+        /// Verstrichene Zeit seit letztem Aufruf
+        /// </summary>
+        private TimeSpan _tc;
+
+        public Derviator() : this(1.0) {
+        }
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="K">Verstärkungsfaktor bzw, Multiplikator</param>
         public Derviator(double K) {
-            this._k = K;
+            _k = K;
         }
 
         public double Calculate(double input) {
             DateTime now = DateTime.Now;
-            this._tc = now - this._last;
-            this._last = now;
-            return Calculate(input, this._tc.TotalMilliseconds);
+            _tc = now - _last;
+            _last = now;
+            return Calculate(input, _tc.TotalMilliseconds);
         }
 
         public double Calculate(double input, double dT) {
-            if (!this._init) {
-                this._init = true;
-                this._old = input;
+            if (!_init) {
+                _init = true;
+                _old = input;
             }
-            else if (this._tc > TimeSpan.Zero) {
-                _out = (input - this._old) / dT * this._k; // laut vorlage in PicoSekunden (1e^-6 µS)
-                this._old = input;
+            else if (_tc > TimeSpan.Zero) {
+                _out = (input - _old)/dT*_k; // laut vorlage in PicoSekunden (1e^-6 µS)
+                _old = input;
             }
 
             return _out;
--- cfcslib/PDController.cs
+++ cfcslib/PDController.cs
@@ -1,16 +1,10 @@
-using System;
-
-using System.Collections.Generic;
-using System.Text;
-
 using Cfcslib.NumMath;
 
 namespace Cfcslib {
     public class PDController {
-        private double _kp = 1.0;
+        private readonly Derviator _diff;
+        private readonly double _kp = 1.0;
         private double _tv = 1.0;
-
-        private Derviator _diff;
 
         public PDController(double kp, double tv) {
             _diff = new Derviator(tv);
@@ -19,7 +13,7 @@ namespace Cfcslib {
 
         public double Calculate(double input) {
             double y = _diff.Calculate(input);
-            y = _kp * (y + input);
+            y = _kp*(y + input);
             return y;
         }
     }
